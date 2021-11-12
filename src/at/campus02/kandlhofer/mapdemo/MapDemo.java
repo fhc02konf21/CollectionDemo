83f41b12a814d0d9ce294a0718a653ae6506c275
dkandlhofer <dkandlhofer@know-center.at>
2021-11-12 20:08:08 +0100

    Daten hinzufügen (put) und iteration

--- src/at/campus02/kandlhofer/mapdemo/MapDemo.java
+++ src/at/campus02/kandlhofer/mapdemo/MapDemo.java
@@ -1,6 +1,8 @@
 package at.campus02.kandlhofer.mapdemo;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MapDemo {
 
@@ -8,6 +10,17 @@ public class MapDemo {
         // HashMap deklarieren
         HashMap<String, Integer> gradeOfStudents = new HashMap<>();
 
+        // Schlüssel und dazugehörigen Wert hinzufügen
+        gradeOfStudents.put("Mustermann", 1);
+        gradeOfStudents.put("Musterfrau", 2);
+        System.out.println("gradeOfStudents = " + gradeOfStudents);
+
+        // Schlüsselwert updaten
+        gradeOfStudents.put("Mustermann", 3);
+        System.out.println("gradeOfStudents = " + gradeOfStudents);
+
+        // Iteration über alle Schlüssel
+        Set<String> students = gradeOfStudents.keySet(); // STRG + ALT + V -> variable automatisch erzeugen
 
     }
 
