e62eb832f3c4c82fa25be999bff3b8584819d6e6
dkandlhofer <dkandlhofer@know-center.at>
2021-11-12 20:02:55 +0100

    HashMap erzeugt

---
+++ src/at/campus02/kandlhofer/mapdemo/MapDemo.java
@@ -0,0 +1,14 @@
+package at.campus02.kandlhofer.mapdemo;
+
+import java.util.HashMap;
+
+public class MapDemo {
+
+    public static void main(String[] args) {
+        // HashMap deklarieren
+        HashMap<String, Integer> gradeOfStudents = new HashMap<>();
+
+
+    }
+
+}
